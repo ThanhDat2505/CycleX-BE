3f49224685be9b193a624a46d1209f5068aa471d
ThanhDat2505 <phanthanhdat2505@gmail.com>
2026-01-19 09:15:49 +0700

    first commit

---
+++ src/main/java/com/example/cyclexbe/CycleXBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyclexbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleXBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycleXBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cyclexbe/CycleXBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyclexbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleXBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
