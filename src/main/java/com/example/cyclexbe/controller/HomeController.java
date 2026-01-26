1362fcfd8f782ca8a627287668c2ad0ed7be25d7
ThanhDat2505 <phanthanhdat2505@gmail.com>
2026-01-26 11:10:22 +0700

    test VM

--- src/main/java/com/example/cyclexbe/controller/HomeController.java
+++ src/main/java/com/example/cyclexbe/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/api/home")
+    @GetMapping("/home")
     public String home() {
         return "Hello Spring Boot";
     }
